8cbd03f3bb67d56d4162926610fcedd84199d2a8
João Victor Macedo Silva <joaovictormacedo@outlook.com>
2020-04-20 03:52:25 -0300

    Create Layout

--- app/src/main/java/br/com/jvmacedo/conversormoedas/MainActivity.java
+++ app/src/main/java/br/com/jvmacedo/conversormoedas/MainActivity.java
@@ -3,12 +3,59 @@ package br.com.jvmacedo.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    /* Attributes */
+    private ViewHolder mViewHolder = new ViewHolder();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate= findViewById(R.id.button_calculate);
+
+        /* Button Calculate */
+       this.mViewHolder.buttonCalculate.setOnClickListener(this);
+       this.clearValues();
+
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.editValue.getText().toString();
+            if("".equals(value)){
+                /* Notifications */
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+            } else {
+            }
+        }
+    }
+
+
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
+
+
     }
 }
